d46cfad4bc6dbaad1bcf178bff6d5091967e7d03
Kristian <kristiantanuwijaya@staff.ubaya.ac.id>
2021-02-01 16:22:38 +0700

    gitlab ci

---
+++ tests/vendor/composer/installed.php
@@ -0,0 +1,78 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f965ad82f4d0197a71165cd9b5c92672d89f8fcd',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f965ad82f4d0197a71165cd9b5c92672d89f8fcd',
+    ),
+    'evenement/evenement' => 
+    array (
+      'pretty_version' => 'v2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6ba9a777870ab49f417e703229d53931ed40fd7a',
+    ),
+    'mpyw/php-hyper-builtin-server' => 
+    array (
+      'pretty_version' => 'v3.1.0',
+      'version' => '3.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '14a24b679b6a1b6af4c21fa05288939bc71bd87e',
+    ),
+    'react/child-process' => 
+    array (
+      'pretty_version' => 'v0.4.3',
+      'version' => '0.4.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b81d06daaafb5d7d592b6f7f8b1b7905cdef9ac6',
+    ),
+    'react/event-loop' => 
+    array (
+      'pretty_version' => 'v0.4.3',
+      'version' => '0.4.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8bde03488ee897dc6bb3d91e4e17c353f9c5252f',
+    ),
+    'react/promise' => 
+    array (
+      'pretty_version' => 'v2.8.0',
+      'version' => '2.8.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f3cff96a19736714524ca0dd1d4130de73dbbbc4',
+    ),
+    'react/stream' => 
+    array (
+      'pretty_version' => 'v0.5.0',
+      'version' => '0.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6ccc4bd601e362aebfe5f449411a08b5f38b229d',
+    ),
+  ),
+);
